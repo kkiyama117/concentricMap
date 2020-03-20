dd2bfb1df7f073511fa052da85aa26c55a004860
kkiyama117 <k.kiyama117@gmail.com>
2020-03-20 17:27:55 +0900

    Upgrade setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 //apply(from = "dependencies.gradle.kts")
 plugins {
     kotlin("jvm") version "1.3.70"
@@ -14,13 +16,12 @@ application {
 //    mainClassName = "io.ktor.server.netty.EngineMain"
 }
 
+// setting for kapt (annotation)
 kapt {
     correctErrorTypes = true
-
     javacOptions {
         option("SomeJavacOption", "OptionValue")
     }
-
     arguments {
         arg("SomeKaptArgument", "ArgumentValue")
     }
@@ -37,27 +38,52 @@ val compileOnly by configurations
 // Versions of plugins
 val ktorVersion by extra { "1.3.2" }
 val serializationVersion by extra { "0.20.0" }
+val autoValueVersion by extra { "1.7" }
+val ktlintVersion by extra { "0.35.0" }
 
 dependencies {
     // implementation
     listOf(
+        // kotlin modules
         kotlin("stdlib-jdk8"),
+        kotlin("reflect"),
+        // kotlin options
+        "org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializationVersion",
         // ktor
-        "io.ktor:ktor-server-netty:$ktorVersion",
-        "org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializationVersion"
-
+        "io.ktor:ktor-server-netty:$ktorVersion"
     ).forEach { implementation(it) }
     // kapt
-    kapt("com.google.auto.value:auto-value:1.6.2")
+    kapt("com.google.auto.value:auto-value:$autoValueVersion")
 //    testImplementation("y.z:x:1.0")
 //    compileOnly("z.x:y:1.0")
 }
 
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
+// tasks
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "1.8"
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions.jvmTarget = "1.8"
+
+/*
+ * KtLint setting (below)
+ */
+val ktlint: Configuration by configurations.creating
+
+dependencies {
+    ktlint("com.pinterest:ktlint:$ktlintVersion")
+}
+
+tasks.register<JavaExec>("ktlint") {
+    group = "verification"
+    description = "Check Kotlin code style."
+    classpath = configurations.getByName("ktlint")
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("src/**/*.kt")
+}
+tasks.register<JavaExec>("ktlintFormat") {
+    group = "verification"
+    description = "Fix Kotlin code style deviations."
+    classpath = configurations.getByName("ktlint")
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("-F", "src/**/*.kt")
 }
