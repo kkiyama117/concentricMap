ba1d92d25daa0770f93078a83c5fff74e0a5fc39
kkiyama117 <k.kiyama117@gmail.com>
2020-03-29 12:07:43 +0900

    Test

--- src/jp/hinatan/Main.kt
+++ src/jp/hinatan/Main.kt
@@ -4,6 +4,5 @@ import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.jetty.Jetty
 
-//fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
+// fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
 fun main(args: Array<String>) = embeddedServer(Jetty, commandLineEnvironment(args)).start(wait = true)
-
